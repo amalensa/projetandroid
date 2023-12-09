79ffde910c3c396dfaef2e054e9bc161e650ddfd
amalensa <153317152+amalensa@users.noreply.github.com>
2023-12-09 15:47:00 +0100

    Add files via upload

---
+++ CookerSignup.java
@@ -0,0 +1,16 @@
+package com.example.tastymix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tastymix.R;
+
+public class CookerSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cooker_signup);
+    }
+}
